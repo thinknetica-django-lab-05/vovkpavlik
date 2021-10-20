ee55dd7ba2a17fba0c3f166c21bcc4f527a01f91
vovkpavlik <vovk.pavlik@bk.ru>
2021-10-20 14:44:28 +0300

    add flatpages to MIDDLEWARE

--- perfectsite/settings.py
+++ perfectsite/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 ]
 
 ROOT_URLCONF = 'perfectsite.urls'
