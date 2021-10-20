3379520c3232c7de410828632e6bc7351ee45a42
vovkpavlik <vovk.pavlik@bk.ru>
2021-10-20 15:59:57 +0300

    hooked up admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.flatpages.admin import FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
 
-# Register your models here.
+
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, FlatPageAdmin)
--- perfectsite/settings.py
+++ perfectsite/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'main',
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
