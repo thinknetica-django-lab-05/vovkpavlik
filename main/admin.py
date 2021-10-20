60a232eeaf509d48fcb1eee710c29857d9d71b17
vovkpavlik <vovk.pavlik@bk.ru>
2021-10-20 23:53:31 +0300

    add FlatPageAdmin to admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,12 @@
+from django import forms
 from django.contrib import admin
+from ckeditor.widgets import CKEditorWidget
 from django.contrib.flatpages.admin import FlatPageAdmin
 from django.contrib.flatpages.models import FlatPage
+
+
+class FlatPageAdmin(admin.ModelAdmin):
+    content = forms.CharField(widget=CKEditorWidget())
 
 
 admin.site.unregister(FlatPage)
