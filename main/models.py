9bba4e6a5c273d6c8dbb3227cce536d8b58284d4
vovkpavlik <vovk.pavlik@bk.ru>
2021-10-22 18:09:12 +0300

    add model Seller

--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Seller(models.model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+
+
+class Category(models.model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(prepopulate_from=('title',))
+
+
+class Tag(models.model):
+    pass
+
+
+class Ads(models.model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
