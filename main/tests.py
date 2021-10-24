4f22f0953496faea1b8bbe98a2cf5b428125351d
vovkpavlik <vovk.pavlik@bk.ru>
2021-10-24 22:42:09 +0300

    add users

--- main/tests.py
+++ main/tests.py
@@ -4,8 +4,13 @@ from django.contrib.auth.models import User
 from .models import Category, Ad, Seller
 
 
+#Create users
+users = ["guest_1", "guest_2", "guest_3"]
+for user in users:
+    User.objects.create_user(user, password=user)
+
+
 #Create sellers
-from django.contrib.auth.models import User
 from main.models import Seller
 users = User.objects.all()
 
