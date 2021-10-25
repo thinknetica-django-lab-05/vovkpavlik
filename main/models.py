b86c9fca2616488823bf78bcc4193cdd067cc8ce
vovkpavlik <vovk.pavlik@bk.ru>
2021-10-25 20:21:17 +0300

    Add 3 ads from guest_1

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
 class Seller(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
 
--- main/tests.py
+++ main/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 from django.contrib.auth.models import User
-from .models import Category, Ad, Seller
+from main.models import Category, Ad, Seller, Tag
 
 
 #Create users
@@ -25,6 +25,33 @@ for category in categories:
 
 
 #Create ads
+user_guest_1 = User.objects.get(username='guest_1')
+user_guest_2 = User.objects.get(username='guest_2')
+user_guest_3 = User.objects.get(username='guest_3')
+
+seller_1 = Seller.objects.get(user=user_guest_1)
+seller_2 = Seller.objects.get(user=user_guest_2)
+seller_3 = Seller.objects.get(user=user_guest_3)
+
+anticvar = Category.objects.get(title='Антиквариат')
+music = Category.objects.get(title='Музыкальное оборудование')
+clothes = Category.objects.get(title='Одежда')
+comp = Category.objects.get(title='Копьютерная техника')
+
+elephant_s_1 = Ad.objects.create(seller=seller_1, category=anticvar, title="Слоновый бивень", description="Большая слоновая кость")
+piano_s_1 = Ad.objects.create(seller=seller_1, category=music, title="Старое фортепиано", description="Досталось от бабушки. Состояние - отличное")
+gun_s_1 = Ad.objects.create(seller=seller_1, category=anticvar, title="Ружье 18 века", description="Не стреляет")
+
+tag_anticvariat = Tag.objects.create(title="Антиквариат")
+tag_exotic = Tag.objects.create(title="Экзотика")
+tag_elephant_bone = Tag.objects.create(title="Слоновая кость")
+tag_old = Tag.objects.create(title="Предметы старины")
+tag_gun = Tag.objects.create(title="Оружие")
+tag_music = Tag.objects.create(title="музыка")
+
+elephant_s_1.tag.set([tag_anticvariat, tag_exotic, tag_elephant_bone])
+piano_s_1.tag.set([tag_anticvariat, tag_old, tag_music])
+tag_music = Tag.objects.create(title="музыка")
 
 
 
