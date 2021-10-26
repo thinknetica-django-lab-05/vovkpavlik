fdafd76701d8c2dacb9de0a4c42fa23cac82b00c
vovkpavlik <vovk.pavlik@bk.ru>
2021-10-26 21:57:36 +0300

    change fild `title` to `name` in BaseModel.

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class BaseModel(models.Model):
-    title = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
 
     class Meta:
         abstract=True
