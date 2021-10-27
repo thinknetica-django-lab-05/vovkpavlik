0600e1a95e7530bfd36919e4ebbd30b99250afa3
vovkpavlik <vovk.pavlik@bk.ru>
2021-10-27 16:23:55 +0300

    add docstrings

--- main/models.py
+++ main/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 
 class Seller(models.Model):
+    """Класс продавца. Вовзращает количество опубликованных объявлений"""
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
 
     @property
@@ -15,6 +16,7 @@ class Seller(models.Model):
 
 
 class Category(models.Model):
+    """Класс категории. Возвращает название категории и слаг"""
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, allow_unicode=True)
 
@@ -26,10 +28,14 @@ class Category(models.Model):
 
 
 class Tag(models.Model):
+    """Класс тэга. Возвращает название тэга"""
     title = models.CharField(max_length=255)
 
 
 class Ad(models.Model):
+    """Класс объявления. Возвращает название объявления, описание,
+категорию и продавца, к которому относится объявление, и тэги
+к этому объявлению + дата создания и изменения объявления"""
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
