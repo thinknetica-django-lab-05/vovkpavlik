6ed3789b24751c535d42db1042bb8ad5c1c0ae4f
vovkpavlik <vovk.pavlik@bk.ru>
2021-10-29 18:27:52 +0300

    add MAINTENANCE_MODE in cotext index function

--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from constance import config
 
 
 def index(request):
     context = {
         "title": "Главная страница",
+        "turn_on_block": config.MAINTENANCE_MODE
     }
     return render(request, 'main/index.html', context)
