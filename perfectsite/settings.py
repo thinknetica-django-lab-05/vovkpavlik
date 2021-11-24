c4aaebeca947d80cfa236ab3f16af45d9586da3d
vovkpavlik <vovk.pavlik@bk.ru>
2021-11-24 19:03:13 +0300

    added SOCIALACCOUNT_PROVIDERS

--- perfectsite/settings.py
+++ perfectsite/settings.py
@@ -170,3 +170,15 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend'
 ]
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
