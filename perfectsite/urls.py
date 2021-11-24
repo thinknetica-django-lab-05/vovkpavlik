f0eded75d333f66fc940ddb33d96e38e606784a6
vovkpavlik <vovk.pavlik@bk.ru>
2021-11-24 19:32:00 +0300

    create directory `account` for `oauth_app`

--- perfectsite/urls.py
+++ perfectsite/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
