1fcc3222bb4df2435c6c5989024b8f1c5b74aef0
vovkpavlik <vovk.pavlik@bk.ru>
2021-11-24 19:02:34 +0300

    added AUTHENTICATION_BACKENDS in settings.py

--- perfectsite/settings.py
+++ perfectsite/settings.py
@@ -165,3 +165,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+]
