f73926a1d19e6fcb9c2311f53138133edf43dcfd
vovkpavlik <vovk.pavlik@bk.ru>
2021-11-25 16:21:05 +0300

    added email_verification in settings.py

--- perfectsite/settings.py
+++ perfectsite/settings.py
@@ -185,6 +185,8 @@ SOCIALACCOUNT_PROVIDERS = {
 SITE_ID = 2
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 LOGIN_REDIRECT_URL = '/accounts/seller/'
 LOGOUT_REDIRECT_URL = '/'
 
