10adee0dfc9fcb26b6ca585a7f2a016eae9905cf
wolfpavel <vovk.pavlik@gmail.com>
2021-12-05 10:57:26 +0300

    added link to current user ads, delete useless code

--- main/urls.py
+++ main/urls.py
@@ -7,7 +7,6 @@ from main.views import AdListView, AdDetailView, SellerUpdateView, AdCreateView,
 urlpatterns = [
     path('', IndexTemplateView.as_view(), name='index'),
     path('ads/', AdListView.as_view(template_name='main/ad_list.html'), name='ad-list'),
-    path('ads/userads/', AdListView.as_view(template_name='main/user_ad_list.html'), name='user-ad-list'),
     path('ads/add/', AdCreateView.as_view(), name='create-ad'),
     path('ads/<slug:slug>/', AdDetailView.as_view(), name='ad-detail'),
     path('ads/<int:pk>/edit/', AdUpdateView.as_view(), name='update_ad'),
