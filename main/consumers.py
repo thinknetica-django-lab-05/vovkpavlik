4b14889b42969180b227fb7de46508c6074222e1
wolfpavel <vovk.pavlik@gmail.com>
2021-12-28 16:35:00 +0300

    added chat to pages

--- main/consumers.py
+++ main/consumers.py
@@ -1,47 +1,31 @@
 import json
-from asgiref.sync import async_to_sync
+
+from django.urls import reverse
+from django.core.exceptions import ObjectDoesNotExist
 from channels.generic.websocket import WebsocketConsumer
+
+from main.models import Ad
 
 
 class ChatConsumer(WebsocketConsumer):
+
     def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
-
-        # Join room group
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name,
-            self.channel_name
-        )
-
         self.accept()
 
     def disconnect(self, close_code):
-        # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name,
-            self.channel_name
-        )
+        pass
 
-    # Receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-
-        # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
-
-    # Receive message from room group
-    def chat_message(self, event):
-        message = event['message']
-
-        # Send message to WebSocket
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+        try:
+            ad = Ad.objects.get(name=message)
+            self.send(text_data=json.dumps({
+                'ad_id': ad.id,
+                'ad_name': ad.name,
+                'ad_url': reverse('ad-detail', kwargs={'slug': int(ad.id)})
+            }))
+        except ObjectDoesNotExist:
+            self.send(text_data=json.dumps({
+                'not_found': "Объявление не найдено",
+            }))
--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from . import views
 
 from main.views import IndexTemplateView
 from main.views import AdListView, AdDetailView, SellerUpdateView
@@ -16,7 +15,5 @@ urlpatterns = [
     path('ads/<slug:slug>/', AdDetailView.as_view(), name='ad-detail'),
     path('ads/<int:pk>/edit/', AdUpdateView.as_view(), name='update_ad'),
     path('accounts/seller/', SellerUpdateView.as_view(), name='seller-info'),
-    path('chat/', views.chat_page, name='chat_page'),
-    path('chat/<str:room_name>/', views.room, name='room')
 
 ]
