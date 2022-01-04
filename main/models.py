915919a813d1b0ec10f927d0d82cd43a0f096b7f
wolfpavel <vovk.pavlik@gmail.com>
2022-01-04 15:43:33 +0300

    added verbose_name for updated_at field in model Ad

--- main/models.py
+++ main/models.py
@@ -110,7 +110,7 @@ class Ad(BaseModel):
     )
     description = models.TextField("Описание")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
-    updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
     tag = models.ManyToManyField(Tag, verbose_name="Тэги")
     price = models.PositiveIntegerField("Цена", default=0)
     is_archive = models.BooleanField("Продано", null=True)
