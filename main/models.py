61b837f38d4a40c7361a08ec75b8ba6cd92f8db2
wolfpavel <vovk.pavlik@gmail.com>
2022-01-06 18:55:22 +0300

    added ArrayField to imports

--- main/models.py
+++ main/models.py
@@ -1,6 +1,7 @@
 import random
 from django.db import models
 from django.contrib.auth.models import User, Group
+from django.contrib.postgres.fields import ArrayField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
