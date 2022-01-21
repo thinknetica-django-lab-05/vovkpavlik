edd948f686421c14cf138651997480a56552f3e2
wolfpavel <vovk.pavlik@gmail.com>
2022-01-21 14:29:14 +0300

    changed Serializer to ModelSerializer. - added DATETIME_FORMAT to settings for DRF

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,31 @@
 from rest_framework import serializers
 
+from main.models import Ad
 
-class AdSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=100)
-    description = serializers.CharField()
-    price = serializers.CharField()
+
+# class AdSerializer(serializers.Serializer):
+#     seller = serializers.CharField(read_only=True)
+#     name = serializers.CharField(max_length=100)
+#     category = serializers.CharField()
+#     tags = serializers.ListField()
+#     description = serializers.CharField()
+#     price = serializers.IntegerField(min_value=0)
+#     created_at = serializers.DateTimeField(read_only=True)
+#     updated_at = serializers.DateTimeField(read_only=True)
+#     is_archive = serializers.BooleanField()
+#
+#     def create(self, validated_data):
+#         return Ad.objects.create(**validated_data)
+#
+
+
+class AdSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
+    seller = serializers.StringRelatedField()
+
+    class Meta:
+        model = Ad
+        fields = [
+            "seller", "name", "category", "tags", "description",
+            "price", "created_at", "updated_at", "is_archive"
+        ]
--- api/views.py
+++ api/views.py
@@ -12,3 +12,11 @@ class AdListView(APIView):
         ads = Ad.objects.all()
         serializer = AdSerializer(ads, many=True)
         return Response({"ads": serializer.data})
+
+    def post(self, request):
+        ad = request.data.get("ad")
+        # Create an article from the above data
+        serializer = AdSerializer(data=ad)
+        if serializer.is_valid(raise_exception=True):
+            ad_saved = serializer.save()
+        return Response({"success": "Article '{}' created successfully".format(ad_saved.title)})
